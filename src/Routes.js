1c8389d2561bb3f579d895abc0f4807e0ab9d293
Jaspal123 <kinggoldy2@hotmail.com>
2020-05-31 13:36:31 +0530

    Completed Authentication and Rolebased Authorization in front end

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import App from './App';
+import Signup from './auth/Signup';
+import Signin from './auth/Signin';
+import Activate from './auth/Activate';
+import Private from './core/Private';
+import Admin from './core/Admin';
+import PrivateRoute from './auth/PrivateRoute';
+import AdminRoute from './auth/AdminRoute';
+const Routes = () => {
+    return(
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App}/>  
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/signin" component={Signin} />
+                <Route exact path="/auth/activate/:token" component={Activate} />
+                <PrivateRoute exact path="/private" component={Private}/>
+                <AdminRoute exact path = "/admin" component={Admin} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes;
